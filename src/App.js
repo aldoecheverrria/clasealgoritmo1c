0beec2415d9fb2f7218796beec71c94fbd5b2256
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-04-16 17:03:12 -0500

    primera fase de algoritmo1c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+      <div className="fixed-header">
+            <nav>
+                <div className="header1">Instructions: read carefully every challenge, and try to solve it, typing the JSX code in the TextArea. press compare, to see the answer. Adittional tips
+                  <ul>
+                    <li>The trick its to keep breathing</li>
+                    <li>After 10min with no clue, google it</li>
+                    <li>After 20min without a good solution, go for an icecream and try later</li>
+                    <li>After 1 day without a good solution, press the "Compare" button, as a last resort</li>
+                  </ul>
+                </div>
+                <div className="header2">Can you solve these JSX Puzzles?</div>
+                <div className="header3">A fun way to increasingly test you problem solving skills</div>
+            </nav>
+    </div>
+    <div className="container">
+    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet sagittis. In tincidunt orci sit amet elementum vestibulum. Vivamus fermentum in arcu in aliquam. Quisque aliquam porta odio in fringilla. Vivamus nisl leo, blandit at bibendum eu, tristique eget risus. Integer aliquet quam ut elit suscipit, id interdum neque porttitor. Integer faucibus ligula.</p>
+        <p>Quis quam ut magna consequat faucibus. Pellentesque eget nisi a mi suscipit tincidunt. Ut tempus dictum risus. Pellentesque viverra sagittis quam at mattis. Suspendisse potenti. Aliquam sit amet gravida nibh, facilisis gravida odio. Phasellus auctor velit at lacus blandit, commodo iaculis justo viverra. Etiam vitae est arcu. Mauris vel congue dolor. Aliquam eget mi mi. Fusce quam tortor, commodo ac dui quis, bibendum viverra erat. Maecenas mattis lectus enim, quis tincidunt dui molestie euismod. Curabitur et diam tristique, accumsan nunc eu, hendrerit tellus.</p>
+    </div>    
+    <div className="fixed-footer">
+        <div className="container">JSX Challenge, by: Aldo Echeverría. Inspired by Hackernoon and the book "Let Us C". May be different way's to solve the same puzzle</div>        
+    </div>
+
+      </body>
     </div>
   );
 }
